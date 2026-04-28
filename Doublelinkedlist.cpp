0393826ddb24342f915be6f77984c4b47c6439ad
xyerakovalskia <xyera123987@gmail.com>
2026-04-28 13:59:19 +0700

    void search data

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -181,3 +181,17 @@ public:
             i--;
         }
     }
+
+    void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        Node *current = START;
