677f63b5a17d6b4becb82de855c5d628561b8749
xyerakovalskia <xyera123987@gmail.com>
2026-04-28 13:38:37 +0700

    step 6 newnode.prev = null

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -48,3 +48,6 @@ public:
             // Step 5: START.prev = newNode (if START exists)
             if (START != NULL)
                 START->prev = newNode;
+
+            // Step 6: newNode.prev = NULL
+            newNode->prev = NULL;
