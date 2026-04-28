0e3db98d41d128641f5180e5337c8efa34528205
xyerakovalskia <xyera123987@gmail.com>
2026-04-28 13:57:47 +0700

    step 2 traverse backward

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -169,3 +169,9 @@ public:
             currentNode = currentNode->next;
             i++;
         }
+
+        // Step 2: Traverse backward
+        cout << "\nRecords in descending order of roll number are:\n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
