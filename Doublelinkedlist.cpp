4536febd38ab3d55a0d9d597266ed613eead3fab
xyerakovalskia <xyera123987@gmail.com>
2026-04-28 13:42:54 +0700

    step 8 locate position for insertion

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -56,3 +56,17 @@ public:
             START = newNode;
             return;
         }
+
+        // insert in between node
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
