804a36f39b7e6d8249646e8b1688709bb1721984
xyerakovalskia <xyera123987@gmail.com>
2026-04-28 13:47:57 +0700

    step 2 if node is at the beginning

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -105,3 +105,13 @@ public:
             cout << "Record not found" << endl;
             return;
         }
+
+        // Step 2: If node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // Step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL; // Step 2b: START.prev = NULL
+        }
+        else
+        {
