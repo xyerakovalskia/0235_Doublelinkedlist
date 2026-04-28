14da9a074990f70513f8784199102ca79dadb125
xyerakovalskia <xyera123987@gmail.com>
2026-04-28 14:00:15 +0700

    step 1 traverse to find matching roll number

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -195,3 +195,7 @@ public:
         cin >> rollNo;
 
         Node *current = START;
+
+        // Step 1: Traverse to find matching roll number
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
